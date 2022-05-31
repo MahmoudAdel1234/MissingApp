552425bc0353c1f096f907441ab58232d7697f0e
mahmoud adel <M777hmoued@gmail.com>
2022-05-31 17:12:41 +0200

    first commit

---
+++ lib/shared/network/end_point.dart
@@ -0,0 +1,68 @@
+// user auth
+String login="/Users/login";
+String signup="/Users/signup";
+String forgetPassword="/Users/forgetPassword";
+String verifyCode="/Users/verifyCode";
+String resetPassword="/Users/resetPassword";
+String testPhoto="/Users/test";
+String logout="/Users/logout";
+
+//home upload
+String uploadMissingPersonUrl="/person/uploadMissingPerson";
+String searchForFamilyUrl="/person/uploadMissingPersonF";
+String uploadThingsUrl="/Things/upload";
+
+// home get
+String getMissingUrl="/person/getMissing";
+String getAllPersonUrl="/person/getAllPerson/All";
+String getOldTenUrl="/person/OldTen";
+String getAllThingsUrl="/Things/getAllThings";
+
+//home filter
+String getAllPersonFilterUrl="/person/getAllPerson/";
+String getAllThingsFilterUrl="/Things/getAllThings";
+
+//user profile
+String getProfileUrl="/Users/profile";
+String contactUsUrl="/contact-us";
+String updateProfileUrl ="/Users/update";
+String updatePasswordUrl ="/Users/updatePassword";
+
+//user case
+String getUserMissingCaseUrl="/person/getMissingByUser";
+String deleteUserMissingCaseUrl="/person/deleteMissing/";
+String getUserSearchForFamilyCaseUrl="/person/getMissingByUserF";
+String deleteSearchForFamilyCaseUrl="/person/deleteMissingF/";
+String getUserThingsCaseUrl="/Things/getThingsForUser";
+String deleteUserThingsCaseUrl="/Things/deleteThingsForUser/";
+
+//update user case
+String updateUserMissingCaseUrl="/person/updateDataofMissing/";
+String updateUserSearchFamilyCaseUrl="/person/updateDataofMissingF/";
+String updateUserThingsCaseUrl="/Things/updateThingsForUser/";
+
+// search by image
+String searchByImageUrl="/person/searchByImage";
+
+// found user case
+String thingsFoundUrl = "/Things/found/";
+String missingFoundUrl = "/person/found/";
+String searchFamilyFoundUrl = "/person/found/";
+String counterFoundUrl = "/person/conterFound";
+
+// admin case
+String getAminMissingCaseUrl="/Admin/getDataAdminPerson?Accept=false";
+String getAminSearchForFamilyCaseUrl="/Admin/getDataAdminPersonF?Accept=false";
+String getAminThingsCaseUrl="/Admin/getDataAdminThings?Accept=false";
+String deleteAdminMissingCaseUrl="/Admin/adminDeletePerson/";
+String deleteAdminSearchForFamilyCaseUrl="/Admin/adminDeletePersonF/";
+String deleteAdminThingsCaseUrl="/Admin/adminDeleteThings/";
+
+String acceptAdminThingsCaseUrl="/Admin/adminAcceptThings/";
+String acceptAdminMissingCaseUrl="/Admin/adminAcceptPerson/";
+String acceptAdminSearchForFamilyCaseUrl="/Admin/adminAcceptPersonF/";
+
+String rejectedAdminSearchForFamilyCaseUrl="/Admin/adminRejectPersonF/";
+String rejectedAdminMissingCaseUrl="/Admin/adminRejectPerson/";
+String rejectedAdminThingsCaseUrl="/Admin/adminRejectThings/";
+
